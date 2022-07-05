c047f8c141820d369063b40eb267caee6e9a9abd
Colin Berry <colinb969@gmail.com>
2022-07-05 16:34:53 +0100

    Migrated from GA to FP Analytics

--- src/ext.ts
+++ src/ext.ts
@@ -1,8 +1,13 @@
 import * as flashpoint from 'flashpoint-launcher';
-import ua from 'universal-analytics';
 import { v4 as uuid } from 'uuid';
 import open from 'open';
+import * as path from 'path';
+import * as fs from 'fs';
 import * as os from 'os';
+import axios from 'axios';
+import { URL } from 'url';
+
+let session: Session | undefined;
 
 export async function activate(context: flashpoint.ExtensionContext) {
   const registerSub = (d: flashpoint.Disposable) => { flashpoint.registerDisposable(context.subscriptions, d)};
@@ -19,17 +24,18 @@ export async function activate(context: flashpoint.ExtensionContext) {
   const firstConnect = true;
   flashpoint.onDidConnect(async () => {
     if (firstConnect) {
+      // First Launch Prompts
       if (firstLaunch) {
         const trackingInfoHuman = [
           'When you launch Flashpoint',
           'How long you use Flashpoint for',
           'The version of Flashpoint you are running',
-          'What games you launch',
+          'What games you launch and how long you spend playing them',
+          'Which URLs games request, to help with finding missing files from games',
           'Simplified Hardware Info (Architecture, Operating System, Available Memory)',
-          'Which URLs games request, to help with repacking legacy games',
         ];
         const res = await flashpoint.dialogs.showMessageBox({
-          message: `Enable the collection of Flashpoint Analytics?\nThese are listed below and configurable on the Config page.\n\n - ${trackingInfoHuman.join('\n - ')}\n\nThis information is tied to a randomized User ID, no personally identifitable information is collected however you can request deletion via the Config page.\n\nAbsolutely NOTHING will be tracked if you click Disable All.\nIf you want to delete this extension to be sure, go to /Data/Extensions and delete the Analytics folder.`,
+          message: `Enable the collection of Flashpoint Analytics? These can help us identify ways to improve the Flashpoint Project.\nThese are listed below and configurable on the Config page.\n\n - ${trackingInfoHuman.join('\n - ')}\n\nThis information is tied to a randomized User ID, no personally identifitable information is collected however you can request deletion via the Config page.\n\nAbsolutely NOTHING will be tracked if you click Disable All.\nIf you want to delete this extension to be sure, go to /Data/Extensions and delete the Analytics folder.`,
           buttons: ['Disable All', 'Enable All'],
           cancelId: 0
         });
@@ -53,11 +59,16 @@ export async function activate(context: flashpoint.ExtensionContext) {
           userId = uuid();
           flashpoint.setExtConfigValue('com.analytics.user-id', userId);
         }
+        
+        // Create Session
+        session = new Session(userId);
+        await session.connect();
+        session.event('Basic', 'launch', '');
+        if (flashpoint.dataVersion) {
+          session.event('Basic', 'version', flashpoint.dataVersion);
+        }
 
-        const visitor = ua('UA-196216429-1', { uid: userId });
-        visitor.event('Basic', 'launch').send();
-        visitor.event('Basic', 'version', flashpoint.dataVersion || '?');
-
+        // Hardware
         if (config.hardware() && !config.hardwareSent()) {
           let totalmem = os.totalmem();
           let simplifiedTotalMem = "unknown";
@@ -66,17 +77,33 @@ export async function activate(context: flashpoint.ExtensionContext) {
           else if (totalmem > 4294900000)  { simplifiedTotalMem = '>= 4GB < 8GB';  }
           else if (totalmem > 2147400000)  { simplifiedTotalMem = '>= 2GB < 4GB';  }
           else if (totalmem > 10000)       { simplifiedTotalMem = '< 2GB';         }
-          visitor.event('Hardware', 'arch', os.arch()).send();
-          visitor.event('Hardware', 'operatingSystem', os.version()).send();
-          visitor.event('Hardware', 'memory', simplifiedTotalMem).send();
-          await flashpoint.setExtConfigValue('com.analytics.hardware-sent', true);
+          Promise.all([
+            session.event('Hardware', 'arch', os.arch()),
+            session.event('Hardware', 'operatingSystem', os.version()),
+            session.event('Hardware', 'memory', simplifiedTotalMem)
+          ])
+          .then(() => {
+            flashpoint.setExtConfigValue('com.analytics.hardware-sent', true);
+          })
+          .catch((err) => {
+            flashpoint.log.error('Error sending Hardware stats, will send on next startup.');
+          })
         }
 
+        // Game Launch
         registerSub(flashpoint.games.onDidLaunchGame((game) => {
           if (config.games()) {
-            visitor.event('Games', 'gameLaunch', game.id).send();
+            session.event('Games', 'gameLaunch', game.id);
+            const timeStart = Date.now();
+            const listener = flashpoint.services.onServiceRemove((process) => {
+              if (process.id === 'game.' + game.id) {
+                session.event('GameTime', 'gameId', ((Date.now() - timeStart) / 1000).toString());
+                flashpoint.dispose(listener);
+              }
+            })
           }
         }));
+
         flashpoint.log.onLog((entry) => {
           if (entry.source === 'Server') {
             let urlSubstring = "";
@@ -94,13 +121,14 @@ export async function activate(context: flashpoint.ExtensionContext) {
               const games = flashpoint.services.getServices().filter(s => s.id.startsWith('game.'));
               if (games.length === 1) {
                 const gameId = games[0].id.substring(5);
-                visitor.event('Repack', 'phpReport', `gameId:${gameId}, path:${urlSubstring}`).send();
+                session.event('Repack', gameId, urlSubstring);
               }
             }
           }
         });
       }
 
+      // Wipe User Data
       registerSub(flashpoint.commands.registerCommand('com.analytics.deletion-request', async () => {
         await flashpoint.setExtConfigValue('com.analytics.basic', false);
         await flashpoint.setExtConfigValue('com.analytics.games', false);
@@ -111,8 +139,82 @@ export async function activate(context: flashpoint.ExtensionContext) {
         userId = uuid();
         await flashpoint.setExtConfigValue('com.analytics.user-id', userId);
         open(deletionFormUrl);
-      }))
+      }));
             
     }
   });
+}
+
+export function deactivate() {
+  if (session) {
+    session.close();
+  }
+}
+
+class Session {
+  private host?: string;
+  private generalToken?: string;
+  private sessionId?: string;
+  private sessionToken?: string;
+  private pingInterval?: NodeJS.Timeout;
+
+  constructor(
+    public userId: string
+  ) {
+    try {
+      const data = JSON.parse(fs.readFileSync(path.join(flashpoint.extensionPath, 'package.json')).toString());
+      this.generalToken = data.config.generalToken;
+      this.host = data.config.host;
+    } catch (err) {
+      flashpoint.log.debug('Failed to read package.json');
+    }
+  }
+  
+  async connect() {
+    if (this.generalToken && this.host) {
+      const url = new URL(`/user/${this.userId}/session`, this.host);
+      const res = await axios.get(url.toString(), {
+        headers: {
+          'Authorization': `Bearer ${this.generalToken}`,
+          'Content-Type': 'application/json'
+        }
+      });
+      if (res.data && res.data.session_uuid && res.data.session_token) {
+        this.sessionId = res.data.session_uuid;
+        this.sessionToken = res.data.session_token;
+        this.pingInterval = setInterval(() => {
+          this.event('Ping', 'alive', '');
+        }, 1000 * 60 * 5);
+        this.event('Ping', 'alive', '');
+        flashpoint.log.debug('Created Analytics Session');
+      } else {
+        flashpoint.log.error('Bad Analytics Response getting Session - \n' + JSON.stringify(res.data));
+      }
+    }
+  }
+
+  close() {
+    if (this.pingInterval) {
+      clearInterval(this.pingInterval);
+      this.sessionId = undefined;
+      this.sessionToken = undefined;
+    }
+  }
+
+  async event(category: string, key: string, value: string) {
+    if (this.generalToken && this.host && this.sessionToken) {
+      const url = new URL(`/session/${this.sessionId}/event`, this.host);
+      const data = { category, key, value };
+      await axios.post(url.toString(), data, {
+        headers: {
+          'Authorization': `Bearer ${this.sessionToken}`,
+          'Content-Type': 'application/json'
+        }
+      })
+      .then(() => {
+        flashpoint.log.debug(`Event: ${JSON.stringify(data)}`);
+      })
+      .catch((err) => { flashpoint.log.error(`Failed to send event - Error: ${err.toString()} - Data: \n` + JSON.stringify(data))});
+    }
+  }
 }
